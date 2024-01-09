5eec19ffb3010bfe7e89069587a227fadcdfdbc5
TekRekon <lukaSiernos@gmail.com>
2024-01-08 20:52:31 -0500

    add getProfRatings function, remove old code

--- rating.js
+++ rating.js
@@ -1,70 +1,63 @@
-const puppeteer = require('puppeteer');
-
-(async () => {
-
-    async function scrapeRating () {
-        const browser = await puppeteer.launch({headless: true});
-        const page = await browser.newPage();
-        const url = 'https://www.ratemyprofessors.com/search/professors/825?q=' + arguments[0] + '%20' + arguments[1];
-        await page.goto(url);
-
-        const noProfs = 'No professors with';
-
-        const noneFound = await page.evaluate((noProfs) => {
-            const pageText = document.body.innerText;
-            return pageText.includes(noProfs);
-          }, noProfs);
-
-          if (noneFound) {
-            console.log('N/A');
-          } else {
-            const quality = 'QUALITY';
-
-            const numProfs = await page.evaluate((quality) => {
-                const pageText = document.body.innerText;
-                const regex = new RegExp(quality, 'g');
-                const matches = pageText.match(regex);
-                return matches ? matches.length : 0;
-              }, quality);
-            if (numProfs == 1) {
-                const hasNoRatings = await page.evaluate(() => {
-                    const numRatings = document.querySelector('.CardNumRating__CardNumRatingCount-sc-17t4b9u-3');
-                    if (numRatings.innerHTML === '0 ratings') {
-                        console.log('N/A');
-                    }
-                  });
-            }
-
-            const highestRating = await page.evaluate(() => {
-                // Get all elements with the specified class name
-                const elements = document.querySelectorAll('.CardNumRating__CardNumRatingNumber-sc-17t4b9u-2');
-    
-                let highest = 0.0;
-    
-                // Loop through the elements and find the highest rating
-                elements.forEach(element => {
-                    const text = element.textContent.trim();
-                    const rating = parseFloat(text);
-                    if (!isNaN(rating) && rating > highest) {
-                        highest = rating;
-                    }
-                });
-    
-                return highest;
-            });
-            console.log(highestRating);
-          }
+import ratings from '@mtucourses/rate-my-professors';
+import * as constants from './constants.js';
 
 
-
-        // const grabRating = await page.evaluate(() => {
-        //     const ratingTag = document.querySelector('.CardNumRating__CardNumRatingNumber-sc-17t4b9u-2');
-        //     return ratingTag.innerHTML;
-        // })
-
-        // console.log(grabRating);
-        await browser.close();
+// @param {string} professorName - The name of the professor to search for
+// @returns {Promise<object>} - An object containing the professor's ratings
+// Example return value:
+// {
+//     avgDifficulty: 4.5,
+//     avgRating: 3.1,
+//     department: 'Computer Science',
+//     firstName: 'David',
+//     id: 'VGVhY2hlci0yMzM2Mjg5',
+//     lastName: 'Menendez',
+//     legacyId: 2336289,
+//     numRatings: 101,
+//     school: {
+//         city: 'New Brunswick',
+//         id: 'U2Nob29sLTgyNQ==',
+//         name: 'Rutgers - State University of New Jersey',
+//         state: 'NJ'
+//     },
+//     wouldTakeAgainPercent: 50
+// }
+async function getProfessorRatings(professorName) {
+    // try {
+    let professors = [];
+    //search for a professor
+    for (let i = 0; i < constants.SCHOOLS.length; i++) {
+        const school = constants.SCHOOLS[i];
+        const schoolId = school[0];
+        professors = await ratings.searchTeacher(professorName, schoolId);
+        if (professors.length > 0) {
+            break;
+        }
     }
-    scrapeRating('ana', 'centeno'); 
+    if (professors.length === 0) {
+        return null;
+    }
 
-})();
+    //get stats for each professor
+    let professor_stats = [];
+    for (let i = 0; i < professors.length; i++) {
+        const professorStats = await ratings.getTeacher(professors[i].id);
+        professor_stats.push(professorStats);
+    }
+    //return most rated professor
+    let max = 0;
+    let maxIndex = 0;
+    for (let i = 0; i < professor_stats.length; i++) {
+        if (professor_stats[i].numRatings > max) {
+            max = professor_stats[i].numRatings;
+            maxIndex = i;
+        }
+    }
+    return professor_stats[maxIndex];
+    // } catch (error) {
+    //     console.error('Failed to retrieve professor ratings:', error);
+    //     return null;
+    // }
+}
+
+export { getProfessorRatings };
