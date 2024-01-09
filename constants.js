d967098a7f02d78c34bab71f9356ace3598a9341
TekRekon <lukaSiernos@gmail.com>
2024-01-08 20:50:11 -0500

    add constants.js

---
+++ constants.js
@@ -0,0 +1,72 @@
+import * as ratings from './rating.js';
+
+//fetch ana centeno
+ratings.getProfessorRatings('ana centeno').then((result) => {
+    console.log(result);
+});
+
+export const CAMPUS_CODES = {
+    NEWARK: 'NK',
+    CAMDEN: 'CM',
+    NEW_BRUNSWICK: 'NB',
+    ONLINE_NEWARK: 'ONLINE_NK',
+    ONLINE_CAMDEN: 'ONLINE_CM',
+    ONLINE_NEW_BRUNSWICK: 'ONLINE_NB',
+};
+
+//sorted by number of professors rated in each school
+export const SCHOOLS = [
+    // Rutgers - State University of New Jersey - New Brunswick - 6407 profs
+    [ 'U2Nob29sLTgyNQ==', 'Rutgers - State University of New Jersey - New Brunswick', 'NB' ],
+
+    // Rutgers - State University of New Jersey - Newark - 1889
+    [ 'U2Nob29sLTgyNg==', 'Rutgers - State University of New Jersey - Newark', 'NK' ],
+
+    // Rutgers - State University of New Jersey - Campus Not Given, Maybe Camden - 1008 profs
+    [ 'U2Nob29sLTgyNA==', 'Rutgers - State University of New Jersey - Camden', ['NK', 'NB', 'CM'] ],
+
+    // Rutgers Law School - Newark - 141 profs
+    [ 'U2Nob29sLTQ5Mzk=', 'Rutgers Law School - Newark', 'NK' ],
+
+    // Rutgers Business School - Newark and New Brunswick - 128 profs
+    [ 'U2Nob29sLTE2ODYy', 'Rutgers Business School - Newark and New Brunswick', ['NK', 'NB'] ],
+
+    // Rutgers - Camden School of Business - 96 profs
+    [ 'U2Nob29sLTExOTY2', 'Rutgers - Camden School of Business', 'CM' ],
+
+    // Rutgers Law School - Camden - 91 profs
+    [ 'U2Nob29sLTQ5OTY=', 'Rutgers Law School - Camden', 'CM' ],
+
+    // Rutgers School of Arts and Sciences - Newark - 38 profs
+    [ 'U2Nob29sLTE4MjE0', 'Rutgers School of Arts and Sciences - Newark', 'NK' ],
+
+    // Rutgers School of Health Related Professions - Likely Newark and New Brunswick - 28 profs
+    [ 'U2Nob29sLTU4MDA=', 'Rutgers School of Health Related Professions', ['NK', 'NB'] ],
+
+    // Rutgers School of Social Work - New Brunswick and Camden? - 20 profs
+    [ 'U2Nob29sLTE3OTk4', 'Rutgers School of Social Work', ['NB', 'CM'] ],
+
+    // Rutgers - Camden - 18 profs
+    [ 'U2Nob29sLTE5MTAw', 'Rutgers- Camden', 'CM' ],
+
+    // Rutgers School of Nursing - New Brunswick and Newark - 17 profs
+    [ 'U2Nob29sLTE3Mjgy', 'Rutgers School of Nursing', ['NB', 'NK'] ],
+
+    // Rutgers Graduate School of Education - New Brunswick - 12 profs
+    [ 'U2Nob29sLTE3MDE3', 'Rutgers Graduate School of Education', 'NB' ],
+
+    // Rutgers School of Dental Medicine - Newark - 9 profs
+    [ 'U2Nob29sLTU5NDc=', 'Rutgers School of Dental Medicine', 'NK' ],
+
+    // Rutgers New Jersey Medical School - Newark - 7 profs
+    [ 'U2Nob29sLTE3Mjc0', 'Rutgers New Jersey Medical School', 'NK' ],
+
+    // Rutgers School of Criminal Justice - Newark - 2 profs
+    [ 'U2Nob29sLTE5MDAw', 'Rutgers School of Criminal Justice', 'NK' ],
+
+    // Rutgers School of Public Affairs and Administration - Newark - 1 prof
+    [ 'U2Nob29sLTE3Mzgz', 'Rutgers School of Public Affairs and Administration', 'NK' ],
+
+    // Rutgers School of Management and Labor Relations - New Brunswick - 0 profs
+    [ 'U2Nob29sLTE3MDg0', 'Rutgers School of Management and Labor Relations', 'NB' ],
+];
